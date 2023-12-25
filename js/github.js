05660c3e5ffee1d54111954e1566f5b2622bd372
ap-rana-w-naim <rana.w.naim@astrapay.com>
2023-12-26 02:32:19 +0700

    - initial commit

--- js/github.js
+++ js/github.js
@@ -1,9 +1,11 @@
 function open_card() {
   document.getElementById('front').className = 'open-card';
+  document.getElementById('inside').className = '';
 }
 
 function close_card() {
   document.getElementById('front').className = '';
+  document.getElementById('inside').className = 'close-card';
 }
 
 var button=document.getElementById("front");
